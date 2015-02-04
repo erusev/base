f90555aa68d5573cd0e4ec842e5f7ed9dfb818c7
Emanuil Rusev <hello@erusev.com>
2015-02-04 02:22:45 +0200

    add src

---
+++ src/Base.php
@@ -0,0 +1,161 @@
+<?php
+
+namespace Base;
+
+use Exception;
+use PDO;
+
+class Base
+{
+    function __construct($dsn, $username = null, $password = null)
+    {
+        $PDO = new PDO($dsn, $username, $password);
+
+        $this->PDO = $PDO;
+    }
+
+    private $PDO;
+
+    function read($statement, array $parameters = array())
+    {
+        $PDOStatement = $this->execute($statement, $parameters);
+
+        $Records = $PDOStatement->fetchAll(PDO::FETCH_ASSOC);
+
+        return $Records;
+    }
+
+    function readField($statement, array $parameters = array())
+    {
+        $PDOStatement = $this->execute($statement, $parameters);
+
+        $Record = $PDOStatement->fetch(PDO::FETCH_COLUMN);
+
+        if ($Record === false)
+        {
+            return;
+        }
+
+        return $Record;
+    }
+
+    function readFields($statement, array $parameters = array())
+    {
+        $PDOStatement = $this->execute($statement, $parameters);
+
+        $fields = $PDOStatement->fetchAll(PDO::FETCH_COLUMN);
+
+        return $fields;
+    }
+
+    function readRecord($statement, array $parameters = array())
+    {
+        $PDOStatement = $this->execute($statement, $parameters);
+
+        $Record = $PDOStatement->fetch(PDO::FETCH_ASSOC);
+
+        if ($Record === false)
+        {
+            return;
+        }
+
+        return $Record;
+    }
+
+    function readItem($table, $id)
+    {
+        $Record = $this->find($table)
+            ->whereEqual('id', $id)
+            ->readRecord();
+
+        return $Record;
+    }
+
+    function update($statement, array $parameters = array())
+    {
+        $PDOStatement = $this->execute($statement, $parameters);
+
+        $impactedRecordCount = $PDOStatement->rowCount();
+
+        return $impactedRecordCount;
+    }
+
+    function updateItem($table, $id, array $Data)
+    {
+        $affectedRecordCount = $this->find($table)
+            ->whereEqual('id', $id)
+            ->update($Data);
+
+        return $affectedRecordCount;
+    }
+
+    /**
+     * @param string $table
+     * @param array $Data
+     * @return string
+     */
+    function createItem($table, array $Data)
+    {
+        $statement = "INSERT INTO `$table` SET";
+
+        $parameters = [];
+
+        foreach ($Data as $name => $value)
+        {
+            $statement .= " `$name` = ?,";
+
+            $parameters []= $value;
+        }
+
+        $statement = substr($statement, 0, - 1);
+
+        $this->execute($statement, $parameters);
+
+        $lastId = $this->lastId();
+
+        return $lastId;
+    }
+
+    function find($table)
+    {
+        $Collection = new Collection($this, $table);
+
+        return $Collection;
+    }
+
+    function execute($statement, array $parameters = array())
+    {
+        $PDOStatement = $this->PDO->prepare($statement);
+
+        $successful = $PDOStatement->execute($parameters);
+
+        if ( ! $successful)
+        {
+            $errorInfo = $PDOStatement->errorInfo();
+
+            throw new Exception($errorInfo[2].': '.$statement);
+        }
+
+        return $PDOStatement;
+    }
+
+    /**
+     * @return string
+     */
+    function lastId()
+    {
+        $lastInsertId = $this->PDO->lastInsertId();
+
+        return $lastInsertId;
+    }
+
+    /**
+     * @return PDO
+     */
+    function pdo()
+    {
+        $PDO = $this->PDO;
+
+        return $PDO;
+    }
+}
---
+++ src/Collection.php
@@ -0,0 +1,386 @@
+<?php
+
+namespace Base;
+
+use Exception;
+
+class Collection
+{
+    /**
+     * @param Base $Base
+     * @param string $table
+     * @throws Exception
+     */
+    function __construct(Base $Base, $table)
+    {
+        $this->Base = $Base;
+        $this->table = $table;
+
+        $this->tableClause = "`$table`";
+        $this->whereClause = '1';
+    }
+
+    private $Base;
+    private $table;
+
+    # ~
+
+    private $parameters = array();
+
+    private $tableClause;
+    private $whereClause;
+    private $limitClause;
+    private $orderClause;
+
+    #
+    # Relationships
+    #
+
+    /**
+     * @param string $table
+     * @param string $foreignKey
+     * @return $this
+     */
+    function has($table, $foreignKey = null)
+    {
+        $foreignKey = $foreignKey ?: $this->table;
+
+        $this->tableClause .= " JOIN `$table` ON `$this->table`.id = `$table`.`$foreignKey`";
+
+        return $this;
+    }
+
+    /**
+     * @param string $table
+     * @param string $foreignKey
+     * @return $this
+     */
+    function belongsTo($table, $foreignKey = null)
+    {
+        $foreignKey = $foreignKey ?: $table;
+
+        $this->tableClause .= " JOIN `$table` ON `$this->table`.`$foreignKey` = `$table`.`id`";
+
+        return $this;
+    }
+
+    /**
+     * @param string $table
+     * @return $this
+     */
+    function hasAndBelongsTo($table)
+    {
+        $tables = array($this->table, $table);
+
+        sort($tables);
+
+        $joinTable = join('_', $tables);
+
+        $this->tableClause .= "
+			JOIN `$joinTable` ON `$this->table`.`id` = `$joinTable`.`$this->table`
+			JOIN `$table` ON `$table`.id = `$joinTable`.`$table`";
+
+        return $this;
+    }
+
+    #
+    # Conditions
+    #
+
+    /**
+     * @param string $condition
+     * @param array $values
+     * @return $this
+     */
+    function where($condition, array $values = array())
+    {
+        $this->whereClause .= " AND $condition";
+
+        foreach ($values as $value)
+        {
+            $this->parameters []= $value;
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param string $field
+     * @param $value
+     * @param bool $reverse
+     * @return $this
+     */
+    function whereEqual($field, $value, $reverse = false)
+    {
+        $field = $this->sanitizePath($field);
+
+        $operator = $reverse ? '!=' : '=';
+
+        $this->whereClause .= " AND $field $operator ?";
+
+        $this->parameters []= $value;
+
+        return $this;
+    }
+
+    /**
+     * @param string $field
+     * @param $value
+     * @return $this
+     */
+    function whereNotEqual($field, $value)
+    {
+        $this->whereEqual($field, $value, true);
+
+        return $this;
+    }
+
+    /**
+     * @param string $field
+     * @param array $values
+     * @param bool $reverse
+     * @return $this
+     */
+    function whereIn($field, array $values, $reverse = false)
+    {
+        $field = $this->sanitizePath($field);
+
+        $operator = $reverse ? 'NOT IN' : 'IN';
+
+        $this->whereClause .= " AND $field $operator (";
+
+        foreach ($values as $value)
+        {
+            $this->whereClause .= '?, ';
+
+            $this->parameters []= $value;
+        }
+
+        $this->whereClause = substr_replace($this->whereClause, ')', - 2);
+
+        return $this;
+    }
+
+    /**
+     * @param string $field
+     * @param array $values
+     * @return $this
+     */
+    function whereNotIn($field, array $values)
+    {
+        $this->whereIn($field, $values, true);
+
+        return $this;
+    }
+
+    /**
+     * @param string $field
+     * @param bool $reverse
+     * @return $this
+     */
+    function whereNull($field, $reverse = false)
+    {
+        $field = $this->sanitizePath($field);
+
+        $operator = $reverse ? 'IS NOT' : 'IS';
+
+        $this->whereClause .= " AND $field $operator NULL";
+
+        return $this;
+    }
+
+    /**
+     * @param string $field
+     * @return $this
+     */
+    function whereNotNull($field)
+    {
+        $this->whereNull($field, true);
+
+        return $this;
+    }
+
+    #
+    # LIMIT
+    #
+
+    /**
+     * @param string $limit
+     * @return $this
+     */
+    function limit($limit)
+    {
+        $this->limitClause = $limit;
+
+        return $this;
+    }
+
+    #
+    # ORDER BY
+    #
+
+    /**
+     * @param string $order
+     * @return $this
+     */
+    function order($order)
+    {
+        $this->orderClause = $order;
+
+        return $this;
+    }
+
+    /**
+     * @param string $field
+     * @return $this
+     */
+    function orderAsc($field)
+    {
+        $field = $this->sanitizePath($field);
+
+        $this->orderClause = $field . ' ASC';
+
+        return $this;
+    }
+
+    /**
+     * @param string $field
+     * @return $this
+     */
+    function orderDesc($field)
+    {
+        $field = $this->sanitizePath($field);
+
+        $this->orderClause = $field . ' DESC';
+
+        return $this;
+    }
+
+    #
+    #
+    # Actions
+    #
+    #
+
+    /**
+     * @param string $selectExpression
+     * @return array
+     */
+    function read($selectExpression = null)
+    {
+        $statement = $this->composeStatement($selectExpression);
+
+        $Records = $this->Base->read($statement, $this->parameters);
+
+        return $Records;
+    }
+
+    /**
+     * @param string $selectExpression
+     * @return array
+     */
+    function readRecord($selectExpression = null)
+    {
+        $statement = $this->composeStatement($selectExpression);
+
+        $Record = $this->Base->readRecord($statement, $this->parameters);
+
+        return $Record;
+    }
+
+    /**
+     * @param string $selectExpression
+     * @return string
+     */
+    function readField($selectExpression = null)
+    {
+        $statement = $this->composeStatement($selectExpression);
+
+        $field = $this->Base->readField($statement, $this->parameters);
+
+        return $field;
+    }
+
+    /**
+     * @param string $selectExpression
+     * @return array
+     */
+    function readFields($selectExpression = null)
+    {
+        $statement = $this->composeStatement($selectExpression);
+
+        $fields = $this->Base->readFields($statement, $this->parameters);
+
+        return $fields;
+    }
+
+    /**
+     * @return int
+     */
+    function count()
+    {
+        $count = (int) $this->readField('COUNT(*)');
+
+        return $count;
+    }
+
+    /**
+     * @param array $Data
+     * @return int
+     */
+    function update(array $Data)
+    {
+        $statement = "UPDATE `$this->table` SET ";
+
+        $fields = array_keys($Data);
+
+        foreach ($fields as $field)
+        {
+            $statement .= "`$this->table`.`$field` = ?, ";
+        }
+
+        $statement = substr_replace($statement, " WHERE $this->whereClause", - 2);
+
+        $this->limitClause and $statement .= " LIMIT $this->limitClause";
+
+        $parameters = array_values($Data);
+        $parameters = array_merge($parameters, $this->parameters);
+
+        $impactedRecordCount = $this->Base->update($statement, $parameters);
+
+        return $impactedRecordCount;
+    }
+
+    #
+    # Private
+    #
+
+    /**
+     * @param string $selectExpression
+     * @return string
+     */
+    private function composeStatement($selectExpression = null)
+    {
+        $selectExpression = $selectExpression ?: "`$this->table`.*";
+
+        $query = "SELECT $selectExpression FROM $this->tableClause WHERE $this->whereClause";
+
+        $this->orderClause and $query .= " ORDER BY $this->orderClause";
+        $this->limitClause and $query .= " LIMIT $this->limitClause";
+
+        return $query;
+    }
+
+    /**
+     * @param string $path
+     * @return string
+     */
+    private function sanitizePath($path)
+    {
+        $path = str_replace('`', '', $path);
+        $path = str_replace('.', '`.`', $path);
+        $path = '`'.$path.'`';
+
+        return $path;
+    }
+}
